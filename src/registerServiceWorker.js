fab2538a2f7018ac0489a2b2e2e6c32d2cc6e4bf
Ashleigh Barnard <ashleighnbarnard@gmail.com>
2018-06-10 12:17:25 -0400

    adding HomePage.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,6 @@
+
+
+/* eslint-disable*/
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -115,3 +118,4 @@ export function unregister() {
     });
   }
 }
+
